b49d3ae8fb8043065faaed0b0a3307a8928c83ce
PavloLiubko <liubko.pavlo.clg@chnu.edu.ua>
2026-03-11 16:22:16 +0300

    Create task2.cpp

---
+++ lab1/task2.cpp
@@ -0,0 +1,84 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "UKR");
+
+	int F[5][5];
+
+	for (int i = 0; i < 5; i++) {
+	
+		for (int j = 0; j < 5; j++) {
+		
+			cout << "Введіть якийсь елемент для матриці:";
+			cin >> F[i][j];
+		
+		}
+	
+	}
+
+	cout << endl << "Наша матриця:" << endl; 
+
+
+	for (int i = 0; i < 5; i++) {
+
+		for (int j = 0; j < 5; j++) {
+
+			cout << F[i][j] << "\t";
+
+		}
+
+		cout << endl;
+
+	}
+
+	int arr[5];
+	for (int i = 0; i < 5; i++) {
+
+		int dobutok = 1;
+		for (int j = 0; j < 5; j++) {
+
+			if (F[i][j] < 0) {
+			
+				dobutok *= F[i][j];
+			
+			}
+
+		}
+
+		arr[i] = dobutok;
+		
+
+
+		cout << endl;
+
+	}
+
+	cout << endl << "Масив з добутків:" << endl;
+	for (int i = 0; i < 5; i++) {
+	
+		cout << arr[i] << "\t";
+
+	}
+
+	int smallest = arr[0];
+	int smallest_i;
+	for (int i = 0; i < 5; i++) {
+
+		if (smallest > arr[i]) {
+			smallest = arr[i];
+			smallest_i = i;
+		}
+
+	}
+
+	cout << endl << "Мінімальний елементр отриманого масиву = " << smallest << " його індекс:" << smallest_i;
+
+
+	return 0;
+
+}
+

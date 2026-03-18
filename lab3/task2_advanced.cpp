cf891523f1e235e67e6bb191b23de56152597a54
PavloLiubko <liubko.pavlo.clg@chnu.edu.ua>
2026-03-18 16:32:18 +0300

    Create task2_advanced.cpp

---
+++ lab3/task2_advanced.cpp
@@ -0,0 +1,101 @@
+#include <iostream> 
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "UKR");
+
+	string S;
+
+	cout << "Введіть якийсь текст(латиницею):";
+	getline(cin, S);
+
+	int words_amount = 1;
+	for (int i = 0; i < S.length(); i++) {
+
+		if (S[i] == ' ') {
+
+			words_amount += 1;
+
+		}
+
+	}
+
+	int* probels = new int[words_amount];
+
+	probels[0] = 0;
+	int k = 1;
+	for (int i = 1; i < S.length(); i++) {
+
+		if (S[i] == ' ') {
+
+			probels[k] = i + 1;
+			k++;
+
+		}
+
+	}
+
+	string* words = new string[words_amount];
+	for (int i = 0; i < words_amount; i++) {
+
+		if (i + 1 < words_amount) {
+			words[i] = S.substr(probels[i], probels[i + 1] - probels[i]);
+		}
+
+		else {
+			words[i] = S.substr(probels[i], S.length() - probels[i]);
+		}
+
+	}
+
+	int* words_length = new int[words_amount];
+
+	for (int i = 0; i < words_amount; i++) {
+
+		words_length[i] = words[i].length();
+
+	}
+
+	words_length[0] -= 1;
+
+	int biggest = words_length[0];
+	int biggest_ind;
+	for (int i = 0; i < words_amount; i++) {
+
+		if (biggest < words_length[i]) {
+
+			biggest_ind = i;
+		}
+
+	}
+
+	int smallest = words_length[0];
+	int smallest_ind;
+	for (int i = 0; i < words_amount; i++) {
+
+		if (smallest > words_length[i]) {
+
+			smallest_ind = i;
+
+		}
+
+	}
+
+	cout << "Текст по словам: \n";
+	for (int i = 0; i < words_amount; i++) {
+	
+		cout << words[i] << endl;
+
+	}
+
+	cout << "Найбільше слово: " << words[biggest_ind] << " кількість символів у найменшому слові = " << words[smallest_ind];
+
+	delete[] probels;
+	delete[] words_length;
+	delete[] words;
+
+	return 0;
+}

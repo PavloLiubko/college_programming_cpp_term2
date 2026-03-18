f15e0cb4f0422b4c0e1b39899e9b6f6b5b171146
PavloLiubko <liubko.pavlo.clg@chnu.edu.ua>
2026-03-18 16:44:21 +0300

    Create task4_advanced.cpp

---
+++ lab3/task4_advanced.cpp
@@ -0,0 +1,37 @@
+#include <iostream> 
+#include <string>
+
+using namespace std;
+
+char ChangeSymbol(char symbol) {
+
+	int code = (int)symbol;
+
+	 code += 2;
+
+	 char letter = code;
+
+	 return letter;
+
+}
+
+
+int main() {
+
+	setlocale(LC_ALL, "UKR");
+
+	string S;
+
+	cout << "Введіть якийсь текст(бажано латиницею):";
+	getline(cin, S);
+
+	for (int i = 0; i < S.length(); i++) {
+	
+		S[i] = ChangeSymbol(S[i]);
+	
+	}
+
+	cout << "Змінений текст:" << S;
+
+	return 0;
+}

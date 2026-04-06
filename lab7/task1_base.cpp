3912f0f3f7e39813b299ae92ec021815f518f74c
PavloLiubko <liubko.pavlo.clg@chnu.edu.ua>
2026-04-06 08:28:22 +0300

    Create task1_base.cpp

---
+++ lab7/task1_base.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+#include <string>
+
+using namespace std;
+
+const float PI = 3.14159;
+
+float FindCylinderVolume(int R, int H) { return 2 * PI * R * H; }
+
+int main() {
+	
+	setlocale(LC_ALL, "UKR");
+
+	float h, r;
+	cout << "Введіть висоту для циліндра:";
+	cin >> h;
+	cin.ignore(1000, '\n');
+	cout << "Введіть радіус для циліндра:";
+	cin >> r;
+	cin.ignore(1000, '\n');
+
+	cout << "Об'єм нашого циліндра:" << FindCylinderVolume(r, h);
+
+	return 0;
+}

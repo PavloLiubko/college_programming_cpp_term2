81509e515835fc8e823673f0098e688bc97a8be6
PavloLiubko <liubko.pavlo.clg@chnu.edu.ua>
2026-04-27 23:36:36 +0300

    Create task1.cpp

---
+++ lab11/task1.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "UKR");
+
+	int rows, columns;
+	cout << "Введіть кількість рядку:";
+	cin >> rows;
+	cout << "Введіть кількість колонок в рядку:";
+	cin >> columns;
+
+
+
+
+	int** A = new int*[rows];
+
+	int** ptr = &A[0];
+	int i = 0;
+
+	while (i < rows) {
+		*ptr = new int[columns];
+		ptr++;
+		i++;
+	}
+
+	int** row = &A[0];
+
+	while (row < &A[0] + rows) {
+
+		int* col = *row;
+
+		while (col < *row + columns) {
+			cout << "Введіть якесь значення для елемента матриці";
+			cin >> *col;   
+			col++;
+		}
+
+		row++;
+	}
+
+	row = &A[0];
+	cout << endl << "Наша матриця: \n";
+
+	while (row < &A[0] + rows) {
+
+		int* col = *row;
+
+		while (col < *row + columns) {
+			cout << '\t' << *col;
+			col++;
+		}
+
+		cout << '\n';
+		row++;
+	}
+
+	int matrix_slid = 0;
+	int row_counter = 0;
+	int columns_counter = 0;
+
+	row = &A[0];
+	while (row < &A[0] + rows) {
+		columns_counter = 0;
+		int* col = *row;
+
+		while (col < *row + columns) {
+
+
+			if (columns_counter == row_counter) {
+			
+				matrix_slid += *col;
+			
+			}
+
+			columns_counter++;
+			col++;
+		}
+
+		row_counter++;
+		row++;
+	}
+
+	row = &A[0];
+	while (row < &A[0] + rows) {
+		columns_counter = 0;
+		int* col = *row;
+
+		while (col < *row + columns) {
+
+
+			if (*col % 2 == 0) {
+
+				*col = matrix_slid;
+
+			}
+
+			col++;
+		}
+
+		row++;
+	}
+
+	row = &A[0];
+	cout << endl << "Наша змінена матриця: \n";
+
+	while (row < &A[0] + rows) {
+
+		int* col = *row;
+
+		while (col < *row + columns) {
+			cout << '\t' << *col;
+			col++;
+		}
+
+		cout << '\n';
+		row++;
+	}
+
+	return 0;
+}

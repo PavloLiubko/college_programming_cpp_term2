ef453c826a43337d23f0af892a6573b190e51c96
PavloLiubko <liubko.pavlo.clg@chnu.edu.ua>
2026-04-28 22:50:26 +0300

    Create task4.cpp

---
+++ lab11/task4.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "UKR");
+
+	int rows, columns;
+	cout << "Введіть кількість рядків для матриці:";
+	cin >> rows;
+	cout << "Введіть кількість стовпців для матриці:";
+	cin >> columns;
+
+	int** A = new int* [rows];
+
+	int** ptr = A;
+	int i = 0;
+
+	while (i < rows) {
+
+		*ptr = new int[columns];
+
+		ptr++;
+		i++;
+
+	}
+
+	int** row = A;
+
+	while (row < A + rows) {
+
+		int* col = *row;
+
+		while (col < *row + columns) {
+
+			cout << "Введіть якесь значення для елемента матриці:";
+			cin >> *col;
+
+			col++;
+
+		}
+
+		row++;
+
+	}
+
+	row = A;
+	cout << "Наша матриця:\n";
+	while (row < A + rows) {
+
+		int* col = *row;
+
+		while (col < *row + columns) {
+
+			cout << '\t' << *col;
+
+			col++;
+
+		}
+		cout << '\n';
+		row++;
+	}
+
+	int* arr = new int[rows * (rows-1) / 2];
+
+	row = A;
+	int row_counter = 0, column_counter = 0, arr_counter = 0;
+	while (row < A + rows) {
+
+		int* col = *row;
+		column_counter = 0;
+		while (col < *row + columns) {
+
+			if (column_counter > row_counter) {
+			
+				arr[arr_counter] = *col;
+				arr_counter++;
+			}
+
+			col++;
+			column_counter++;
+		}
+		row++;
+		row_counter++;
+	}
+
+
+	int minimum = arr[0];
+
+	for (int i = 0; i < arr_counter; i++) {
+	
+		if (minimum > arr[i]) {
+		
+			minimum = arr[i];
+		
+		}
+	
+	}
+
+
+	cout << endl << minimum;
+
+
+	return 0;
+}
+
+// must be changed

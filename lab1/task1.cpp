913812e37431dca34931be339088d0c003252ada
PavloLiubko <liubko.pavlo.clg@chnu.edu.ua>
2026-04-30 23:55:09 +0300

    Update task1.cpp

--- lab1/task1.cpp
+++ lab1/task1.cpp
@@ -4,33 +4,33 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "UKR");
-    int m, n;
-    cout << "Введіть кількість рядків матриці:";
-    cin >> m;
-    cout << "Введіть кількість стовпчиків матриці:";
-    cin >> n;
+	setlocale(LC_ALL, "UKR");
+	int m, n;
+	cout << "Введіть кількість рядків матриці:";
+	cin >> m;
+	cout << "Введіть кількість стовпчиків матриці:";
+	cin >> n;
 
-	int** A = new int*[m];
-	
+	int** A = new int* [m];
+
 	for (int i = 0; i < m; i++) {
-	    
-	    A[i] = new int[n];
-	    
+
+		A[i] = new int[n];
+
 	}
 
 	for (int i = 0; i < m; i++) {
 		for (int j = 0; j < n; j++) {
-		
+
 			cout << "Введіть якесь значення для матриці:";
 			cin >> A[i][j];
 
 		}
 	}
 
-	cout << "Наша матриця: \n"; 
-	for (int i = 0; i < 6; i++) {
-		for (int j = 0; j < 4; j++) {
+	cout << "Наша матриця: \n";
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
 
 			cout << A[i][j] << "\t";
 
@@ -41,13 +41,13 @@ int main()
 
 	int biggest = A[0][0];
 	int biggest_i, biggest_j;
-	for (int i = 0; i < 6; i++) {
-		for (int j = 0; j < 4; j++) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
 
 			if (A[i][j] > biggest) {
 				biggest = A[i][j];
 				biggest_i = i;
-				biggest_j = j; 
+				biggest_j = j;
 			}
 
 		}
@@ -55,4 +55,5 @@ int main()
 
 	cout << "Найбільший елемент матриці = " << biggest << " його координати: " << biggest_i << " " << biggest_j;
 
-
+	return 0;
+}

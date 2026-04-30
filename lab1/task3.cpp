72fefe47af630ba7e5a44eb7117b5d246dfb7e34
PavloLiubko <liubko.pavlo.clg@chnu.edu.ua>
2026-04-30 12:20:02 +0300

    Update task3.cpp

--- lab1/task3.cpp
+++ lab1/task3.cpp
@@ -8,7 +8,7 @@ int faktorial(int q) {
 	int result = 0;
 	for (int i = 1; i <= q; i++) { result += i; }
 	return result;
-	
+
 }
 
 int main() {
@@ -20,24 +20,24 @@ int main() {
 	cin >> n;
 
 
-	int** G = new int*[n];
+	int** G = new int* [n];
 
 	for (int i = 0; i < n; i++) {
-	
+
 		int* row = new int[n];
 		G[i] = row;
-	
+
 	}
 
 	for (int i = 0; i < n; i++) {
-	
+
 		for (int j = 0; j < n; j++) {
 
 			cout << endl << "Введіть якесь значення для елементів матриці:";
 			cin >> G[i][j];
-		
+
 		}
-	
+
 	}
 
 	for (int i = 0; i < n; i++) {
@@ -53,48 +53,50 @@ int main() {
 
 	int len = faktorial(n);
 	int* arr_meanings = new int[len];
-    int* arr_indexes1 = new int[len];
-    int* arr_indexes2 = new int[len];
+	int* arr_indexes1 = new int[len];
+	int* arr_indexes2 = new int[len];
 	int pointer = 0;
 	for (int i = 0; i < n; i++) {
-	
+
 		for (int j = 0; j < n; j++) {
-		
+
 			if (j < i && G[i][j] > 0) {
-			
+
 				arr_meanings[pointer] = G[i][j];
-				arr_indexes1[pointer] = i ;
-				arr_indexes2[pointer] = j ;
+				arr_indexes1[pointer] = i;
+				arr_indexes2[pointer] = j;
 				pointer += 1;
-			
+
 			}
-		
+
 		}
 
 	}
 
 	cout << "Наш утворений масив: \n";
 	for (int i = 0; i < n; i++) {
-	
+
 		cout << arr_meanings[i] << '\t';
 	}
 
 	int maximum = arr_meanings[0];
-	int maximum_i, maximum_j;
+	int maximum_i = arr_indexes1[0];
+	int maximum_j = arr_indexes2[0];
 	int minimum = arr_meanings[0];
-    int minimum_i, minimum_j;
+	int minimum_i = arr_indexes1[0];
+	int minimum_j = arr_indexes2[0];
 
 	for (int i = 0; i < len; i++) {
-	
-		if (arr_meanings[i] > minimum && arr_meanings[i] > 0) {
-		
+
+		if (arr_meanings[i] > maximum && arr_meanings[i] > 0) {
+
 			maximum = arr_meanings[i];
 			maximum_i = arr_indexes1[i];
 			maximum_j = arr_indexes2[i];
-	
-		}
+
+		}	
 		else if (arr_meanings[i] < minimum && arr_meanings[i] > 0) {
-		
+
 			minimum = arr_meanings[i];
 			minimum_i = arr_indexes1[i];
 			minimum_j = arr_indexes2[i];
@@ -103,11 +105,12 @@ int main() {
 
 	}
 
-	cout << endl << "Максимальний елемент = " << maximum << " з координатами" << maximum_i << " " << maximum_j <<  endl << "Мінімальний елемент = "  << minimum << " з координатами " << minimum_i << " " << minimum_j;
+	cout << endl << "Максимальний елемент = " << maximum << " з координатами " << maximum_i << " " << maximum_j << endl << "Мінімальний елемент = " << minimum << " з координатами " << minimum_i << " " << minimum_j;
 
 	delete[] G;
-	
-	return 0;
+	delete[] arr_meanings;
+	delete[] arr_indexes1;
+	delete[] arr_indexes2;
 
-	// must be changed
-} 
+	return 0;
+}

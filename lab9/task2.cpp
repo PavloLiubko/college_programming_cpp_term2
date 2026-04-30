2433f31146837aae2f3f53d876c751c3d44287bb
PavloLiubko <liubko.pavlo.clg@chnu.edu.ua>
2026-05-01 01:17:33 +0300

    Update task2.cpp

--- lab9/task2.cpp
+++ lab9/task2.cpp
@@ -13,7 +13,7 @@ void main()
 	long int* pal = &al, * pbl = &bl;
 
 	cout << "\n Значення і різниця вказівників: ";
-	cout << " \n npac =  " << (void*)pac << "/tpbc= " << (void*)pbc;
+	cout << " \n npac =  " << (void*)pac << "\t pbc= " << (void*)pbc;
 	cout << "\t (pac-pbc)= " << (pac - pbc);
 	cout << "\n pal=" << pal << "\t pbl = " << pbl << "\t (pbl-pal)= " << (pbl - pal);
 	cout << "Різниця числових значень вказівників:";

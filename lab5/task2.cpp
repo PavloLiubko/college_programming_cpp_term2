7b552876ccb3d4814d921637be47112135c66d95
PavloLiubko <liubko.pavlo.clg@chnu.edu.ua>
2026-05-01 00:47:46 +0300

    Update task2.cpp

--- lab5/task2.cpp
+++ lab5/task2.cpp
@@ -10,13 +10,13 @@ void BubbleSort(Any arr[], int n) {
 	for (int i = 0; i < n - i; i++) {
 		bool swapped = false;
 		for (int j = 0; j < n - i - 1; j++) {
-		
+
 			arr[j].road_time > arr[j + 1].road_time;
 			Any Temporary = arr[j];
-			arr[j] = arr[j+1];
+			arr[j] = arr[j + 1];
 			arr[j + 1] = Temporary;
 			swapped = true;
-			
+
 		}
 		if (!swapped) break;
 	}
@@ -38,7 +38,7 @@ int main() {
 		int road_time; //  в хвилинах
 
 	};
-	
+
 	int schedules_amount;
 	cout << "Введіть кількість потягів з розкладом:";
 	cin >> schedules_amount;
@@ -46,7 +46,7 @@ int main() {
 	Train* schedules = new Train[schedules_amount];
 
 	for (int i = 0; i < schedules_amount; i++) {
-		
+
 		cout << "Введіть номер потяга:";
 		cin >> schedules[i].number;
 		cin.ignore(1000, '\n');
@@ -74,11 +74,11 @@ int main() {
 
 	cout << "Номер поїзду, що має найтриваліший час шляху:" << schedules[0].number;
 
-	cout << "Потяги, які підходять умовам задачі: \n";
+	cout << endl << "Потяги, які підходять умовам задачі: \n";
 	for (int i = 0; i < schedules_amount; i++) {
-	
-		if ( schedules[i].destination == "Kyiv" || schedules[i].destination == "Київ" && schedules[i].start_time[0] >= 6 && schedules[i].arriving_time[0] <= 10) {
-		
+
+		if (schedules[i].destination == "Kyiv" || schedules[i].destination == "Київ" && schedules[i].start_time[0] >= 6 && schedules[i].arriving_time[0] <= 10) {
+
 			cout << endl << "Номер потяга:" << schedules[i].number;
 			cout << endl << "Станція призначення(місто):" << schedules[i].destination;
 			cout << endl << "Довжина шляху потяга(км):" << schedules[i].road_length;
@@ -87,9 +87,9 @@ int main() {
 			cout << endl << "Година прибуття потяга:" << schedules[i].arriving_time[0];
 			cout << endl << "Хвилини прибуття потяга:" << schedules[i].arriving_time[1];
 			cout << endl << "Час шляху потяга:" << schedules[i].road_time;
-		
+
 		}
-	
+
 	}
 
 	delete[] schedules;

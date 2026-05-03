8e920cf2dabbb8e7ac5ed0ba51f48b0e3d88b990
PavloLiubko <liubko.pavlo.clg@chnu.edu.ua>
2026-05-03 12:26:42 +0300

    Create task3.cpp

---
+++ lab14/task3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    setlocale(LC_ALL, "UKR");
+
+    struct Node {
+
+        int Data;
+        Node* next;
+        int ID;
+
+    };
+
+    Node* a = new Node{ 1, nullptr, 1 };
+    Node* b = new Node{ 2, a, 2 };
+    Node* c = new Node{ 3,  nullptr, 3 };
+    a->next = c;
+
+    cout << "\n Наш список: \n";
+    Node *current = b;
+    while (current != nullptr) {
+
+        cout << current->Data << "->";
+        current = current->next;
+    }
+    cout << "nullptr";
+
+    current = b;
+    while (current != nullptr) {
+
+        Node* temp = current-> next;
+        delete current;
+        current = temp;
+    }
+
+
+    return 0;
+}

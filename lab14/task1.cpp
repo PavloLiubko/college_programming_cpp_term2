e01e99e5b11f2230caad8b49ac121932ba792d56
PavloLiubko <liubko.pavlo.clg@chnu.edu.ua>
2026-05-03 11:34:01 +0300

    Update task1.cpp

--- lab14/task1.cpp
+++ lab14/task1.cpp
@@ -2,40 +2,42 @@
 using namespace std;
 int main() {
 
+    setlocale(LC_ALL, "UKR");
+
     struct Node {
-        
+
         int Data;
         Node* next;
-        
+
     };
-    
-    Node* head = new Node {1, nullptr};
-    Node* second = new Node {2, nullptr};
+
+    Node* head = new Node{ 1, nullptr };
+    Node* second = new Node{ 2, nullptr };
     head->next = second;
-    Node* third = new Node {3, nullptr};
+    Node* third = new Node{ 3, nullptr };
     second->next = third;
-    Node* tale = new Node {4, nullptr};
+    Node* tale = new Node{ 4, nullptr };
     third->next = tale;
-    
-   cout << "Сума передостаннього та останнього елементів:" << third->Data + tale->Data;
-   
-   Node* temp = head;
-   head = head->next;
-   delete temp;
-   
-   int counter = 0;
-   Node* current = head;    
+
+    cout << "Сума передостаннього та останнього елементів:" << third->Data + tale->Data;
+
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+
+    int counter = 0;
+    Node* current = head;
     while (current != nullptr) {
-       if (current->Data %2 == 0)  {
-           counter++; 
-           
-       }
-       current = current->next;
+        if (current->Data % 2 == 0) {
+            counter++;
+
+        }
+        current = current->next;
     }
-    
+
     cout << "\n Кількість парних елементів = " << counter;
-   
-    current = head;    
+
+    current = head;
     while (current != nullptr) {
         Node* temp = current;
         current = current->next;

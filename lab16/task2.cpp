9a6efc96a228d068bfe0be3ce3b555c3f0d3dd47
PavloLiubko <liubko.pavlo.clg@chnu.edu.ua>
2026-05-04 18:03:30 +0300

    Create task2.cpp

---
+++ lab16/task2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "UKR");
+
+    int n;
+    cout << "Введіть якесь ціле число:";
+    cin >> n;
+
+    if (n <= 0) return 0;
+
+    int* arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+
+        arr[i] = 0;
+    }
+
+    int counter = 0;
+    for (int i = 1; i < n; i++) {
+    
+        if (n % i == 0) {
+        
+            arr[counter] = i;
+            counter++;
+        }
+    
+    }
+
+
+    ofstream file("lab16.txt", ios_base::out);
+
+    for (int i = 0; i < n; i++) {
+
+        if (file.is_open()) {
+        
+            if (arr[i] != 0) {
+            
+                file << arr[i];
+            
+            }
+
+        }
+
+    }
+
+    delete[] arr;
+
+    return 0;
+}
